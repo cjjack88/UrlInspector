269977653cd949915a1da321b32f6d1ba420f105
Chan Chen Jie <jie26488@hotmail.com>
2014-04-30 03:27:44 +0800

    Upload solution

---
+++ src/UrlInspector/UrlInspector/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace UrlInspector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.windowsCheck = new System.Windows.Forms.CheckBox();
+            this.pageCheck = new System.Windows.Forms.CheckBox();
+            this.OpenFolderBtn = new System.Windows.Forms.Button();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.inputTb = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.resultDataGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.windowsCheck);
+            this.groupBox1.Controls.Add(this.pageCheck);
+            this.groupBox1.Controls.Add(this.OpenFolderBtn);
+            this.groupBox1.Controls.Add(this.StartBtn);
+            this.groupBox1.Controls.Add(this.inputTb);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 213);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tool";
+            // 
+            // windowsCheck
+            // 
+            this.windowsCheck.AutoSize = true;
+            this.windowsCheck.Checked = true;
+            this.windowsCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.windowsCheck.Location = new System.Drawing.Point(166, 184);
+            this.windowsCheck.Name = "windowsCheck";
+            this.windowsCheck.Size = new System.Drawing.Size(133, 17);
+            this.windowsCheck.TabIndex = 4;
+            this.windowsCheck.Text = "Screenshoot Windows";
+            this.windowsCheck.UseVisualStyleBackColor = true;
+            // 
+            // pageCheck
+            // 
+            this.pageCheck.AutoSize = true;
+            this.pageCheck.Location = new System.Drawing.Point(13, 184);
+            this.pageCheck.Name = "pageCheck";
+            this.pageCheck.Size = new System.Drawing.Size(148, 17);
+            this.pageCheck.TabIndex = 3;
+            this.pageCheck.Text = "Screenshoot Whole Page";
+            this.pageCheck.UseVisualStyleBackColor = true;
+            // 
+            // OpenFolderBtn
+            // 
+            this.OpenFolderBtn.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.OpenFolderBtn.Location = new System.Drawing.Point(417, 184);
+            this.OpenFolderBtn.Name = "OpenFolderBtn";
+            this.OpenFolderBtn.Size = new System.Drawing.Size(141, 23);
+            this.OpenFolderBtn.TabIndex = 2;
+            this.OpenFolderBtn.Text = "Screen Shoot Folder";
+            this.OpenFolderBtn.UseVisualStyleBackColor = true;
+            this.OpenFolderBtn.Click += new System.EventHandler(this.OpenFolderBtn_Click);
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(336, 184);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 1;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // inputTb
+            // 
+            this.inputTb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTb.Location = new System.Drawing.Point(13, 19);
+            this.inputTb.Multiline = true;
+            this.inputTb.Name = "inputTb";
+            this.inputTb.Size = new System.Drawing.Size(763, 159);
+            this.inputTb.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.resultDataGrid);
+            this.groupBox2.Location = new System.Drawing.Point(12, 232);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(777, 397);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // resultDataGrid
+            // 
+            this.resultDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultDataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultDataGrid.Location = new System.Drawing.Point(3, 16);
+            this.resultDataGrid.Name = "resultDataGrid";
+            this.resultDataGrid.Size = new System.Drawing.Size(771, 378);
+            this.resultDataGrid.TabIndex = 0;
+            this.resultDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.resultDataGrid_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(718, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "@CJ Chan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 641);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.resultDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.TextBox inputTb;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView resultDataGrid;
+        private System.Windows.Forms.Button OpenFolderBtn;
+        private System.Windows.Forms.CheckBox windowsCheck;
+        private System.Windows.Forms.CheckBox pageCheck;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ src/UrlInspector/UrlInspector/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics.Contracts;
+using System.IO;
+
+
+using System.Windows.Forms;
+using System.Web;
+using System.Drawing;
+using System.Threading;
+using Coypu;
+using Coypu.Drivers.Selenium;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace UrlInspector
+{
+    public partial class Form1 : Form
+    {
+        public struct UrlInfo
+        {
+            public string Url { set; get; }
+            public bool IsAccessible { set; get; }
+            public string Remark { set; get; }
+            public string Content { set; get; }
+        }
+        public string _myDocumentFolderPath = "";
+        public string MyDocumentFolderPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_myDocumentFolderPath))
+                    _myDocumentFolderPath = string.Format(@"{0}\{1}\", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"UrlInspector");
+
+                FileInfo info = new FileInfo(_myDocumentFolderPath);
+                if (!info.Directory.Exists)
+                    System.IO.Directory.CreateDirectory(info.DirectoryName);
+
+                return _myDocumentFolderPath;
+            }
+        }
+
+
+        WebBrowser browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+
+            var sessionConfiguration = new SessionConfiguration();
+
+            sessionConfiguration.Driver = typeof(SeleniumWebDriver);
+            sessionConfiguration.Browser = Coypu.Drivers.Browser.Firefox;
+
+            using (var browser = new BrowserSession(sessionConfiguration))
+            {
+                browser.MaximiseWindow();
+                List<UrlInfo> urlList = new List<UrlInfo>();
+
+                UrlInfo url;
+                Uri theUri;
+                foreach (string theUrlString in inputTb.Lines)
+                {
+                    url = new UrlInfo();
+                    url.Url = theUrlString;
+                    url.Content = "";
+                    url.IsAccessible = false;
+                    theUri = null;
+                    bool isUri = Uri.TryCreate(theUrlString, UriKind.Absolute, out theUri);
+                    if (!isUri)
+                    {
+                        url.IsAccessible = false;
+                        url.Remark = "Not a valid URL";
+                        urlList.Add(url);
+                        continue;
+                    }
+
+                    bool IsRespond = isValidURL(theUrlString);
+
+                    if (!IsRespond)
+                    {
+                        url.Remark = "No response from server";
+                        urlList.Add(url);
+                        continue;
+                    }
+                    url.IsAccessible = true;
+                    //browser.Navigate(theUri);
+                    url.Remark = "Accessible";
+                    //url.Content = new WebClient().DownloadString(theUri);
+
+                    browser.Visit(theUrlString);
+                    url.Content = browser.Text;
+                    url.Remark = browser.Title;
+
+                    if (pageCheck.Checked)
+                        browser.SaveScreenshot(string.Format("{0}{1}.jpeg", MyDocumentFolderPath, DateTime.Now.ToString("ddMMyyyyhhmmsstt")), ImageFormat.Jpeg);
+
+                    if (windowsCheck.Checked)
+                        PrintScreen(string.Format("{0}{1}.jpeg", MyDocumentFolderPath, DateTime.Now.ToString("ddMMyyyyhhmmsstt")));
+
+                    urlList.Add(url);
+                }
+
+                resultDataGrid.DataSource = urlList;
+            }
+        }
+
+        public void PrintScreen(string path)
+        {
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+            printscreen.Save(path, ImageFormat.Jpeg);
+        }
+
+        public bool isValidURL(string url)
+        {
+            WebRequest webRequest = WebRequest.Create(url);
+            WebResponse webResponse;
+            try
+            {
+                webResponse = webRequest.GetResponse();
+            }
+            catch //If exception thrown then couldn't get response from address
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void resultDataGrid_CellContentClick(object sender, System.Windows.Forms.DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void OpenFolderBtn_Click(object sender, EventArgs e)
+        {
+            Process.Start(MyDocumentFolderPath);
+        }
+    }
+}
